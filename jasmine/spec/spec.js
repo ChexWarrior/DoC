5a92c48a6a1bfc387171949e9d35bdfd8b2395de
Andrew Lehman <aplehm@gmail.com>
2017-02-27 21:28:07 -0500

    Remove test

--- jasmine/spec/spec.js
+++ jasmine/spec/spec.js
@@ -1,50 +1,5 @@
 describe('A new deck', function() {
 
-  it('can be shuffled on initialization', function(done) {
-    var callback = function(deck) {
-      expect(deck.shuffled).toBe(true);
-      done();
-    };
+  
 
-    DoC.createDeck(callback, true);
-  });
-
-  it('can contain multiple decks', function(done) {
-    var callback = function(deck) {
-      expect(deck.remaining).toBe(156);
-      done();
-    };
-
-    DoC.createDeck(callback, false, [], 3);
-  });
-
-  it('can contain a partial amount of cards', function(done) {
-    var callback = function(deck) {
-      expect(deck.remaining).toBe(4);
-      done();
-    };
-
-    var cardsToAdd = [ 'AS', 'AC', 'AD', 'AH' ];
-    DoC.createDeck(callback, false, cardsToAdd);
-  });
-
-  it('cannot contain more than one of the same card', function(done) {
-    var callback = function(deck) {
-      expect(deck.remaining).toBe(3);
-      done();
-    };
-
-    var cardsToAdd = [ 'AS', 'AS', 'AD', 'AH' ];
-    DoC.createDeck(callback, false, cardsToAdd);
-  });
-
-  it('initialized as a partial deck and multiple decks only includes partial cards', function(done) {
-    var callback = function(deck) {
-      expect(deck.remaining).toBe(3);
-      done();
-    };
-
-    var cardsToAdd = ['AS', 'AD', 'AH'];
-    DoC.createDeck(callback, false, cardsToAdd, 3);
-  });
 });
