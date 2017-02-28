f2d3a6f54bcf29c8ae37be3e942b822d40888c86
Andrew Lehman <aplehm@gmail.com>
2017-02-28 07:33:44 -0500

    Refactor tests

--- jasmine/spec/spec.js
+++ jasmine/spec/spec.js
@@ -1,50 +1,54 @@
+
 describe('A new deck', function() {
+  var callbacks = {};
+  var parameters = {};
+  var resetParameters = function() {
+    callbacks = {};
+    parameters = {};
+  };
+
 
   it('can be shuffled on initialization', function(done) {
-    var callback = function(deck) {
+    callbacks.success = function(deck) {
       expect(deck.shuffled).toBe(true);
       done();
     };
 
-    DoC.createDeck(callback, true);
+    parameters.shuffle = true;
+    DoC.createDeck(callbacks, parameters);
   });
 
   it('can contain multiple decks', function(done) {
-    var callback = function(deck) {
+    resetParameters();
+    callbacks.success = function(deck) {
       expect(deck.remaining).toBe(156);
       done();
     };
 
-    DoC.createDeck(callback, false, [], 3);
+    parameters.numDecks = 3;
+    DoC.createDeck(callbacks, parameters);
   });
 
   it('can contain a partial amount of cards', function(done) {
-    var callback = function(deck) {
+    resetParameters();
+    callbacks.success = function(deck) {
       expect(deck.remaining).toBe(4);
       done();
     };
 
-    var cardsToAdd = [ 'AS', 'AC', 'AD', 'AH' ];
-    DoC.createDeck(callback, false, cardsToAdd);
-  });
-
-  it('cannot contain more than one of the same card', function(done) {
-    var callback = function(deck) {
-      expect(deck.remaining).toBe(3);
-      done();
-    };
-
-    var cardsToAdd = [ 'AS', 'AS', 'AD', 'AH' ];
-    DoC.createDeck(callback, false, cardsToAdd);
+    parameters.partialDeck = [ 'AS', 'AC', 'AD', 'AH' ];
+    DoC.createDeck(callbacks, parameters);
   });
 
   it('initialized as a partial deck and multiple decks only includes partial cards', function(done) {
-    var callback = function(deck) {
+    resetParameters();
+    callbacks.success = function(deck) {
       expect(deck.remaining).toBe(3);
       done();
     };
 
-    var cardsToAdd = ['AS', 'AD', 'AH'];
-    DoC.createDeck(callback, false, cardsToAdd, 3);
+    parameters.partialDeck = ['AS', 'AD', 'AH'];
+    parameters.numDecks = 3;
+    DoC.createDeck(callbacks, parameters);
   });
 });
