5f6486e2f49aa457d43bd8285e939f0db38cfe76
Andrew Lehman <aplehm@gmail.com>
2017-02-27 20:42:17 -0500

    Add tests for deck creation

--- jasmine/spec/spec.js
+++ jasmine/spec/spec.js
@@ -1,6 +1,6 @@
-describe('A new deck of cards', function() {
+describe('A new deck', function() {
 
-  it('can be shuffled', function(done) {
+  it('can shuffle the deck on creation', function(done) {
     var callback = function(deck) {
       expect(deck.shuffled).toBe(true);
       done();
@@ -8,4 +8,35 @@ describe('A new deck of cards', function() {
 
     DoC.createDeck(callback, true);
   });
+
+  it('can contain multiple decks', function(done) {
+    var callback = function(deck) {
+      expect(deck.remaining).toBe(156);
+      done();
+    };
+
+    DoC.createDeck(callback, false, [], 3);
+  });
+
+  it('can contain a partial amount of cards', function(done) {
+    var callback = function(deck) {
+      expect(deck.remaining).toBe(4);
+      done();
+    };
+
+    var cardsToAdd = [ 'AS', 'AC', 'AD', 'AH' ];
+    DoC.createDeck(callback, false, cardsToAdd);
+  });
+
+  it('cannot contain more than one of the same card', function(done) {
+    var callback = function(deck) {
+      expect(deck.remaining).toBe(3);
+      done();
+    };
+
+    var cardsToAdd = [ 'AS', 'AS', 'AD', 'AH' ];
+    DoC.createDeck(callback, false, cardsToAdd);
+  });
+
+  
 });
