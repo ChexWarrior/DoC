65a38d52fbf02a7ae877dc9ecba58b3b8d85a993
Andrew Lehman <aplehm@gmail.com>
2017-03-02 18:46:46 -0500

    Revert "Remove old tests"
    This reverts commit 254714fa6b96309796f5fcf5abee85a6c526c126.

---
+++ jasmine/spec/spec.js
@@ -0,0 +1,88 @@
+
+describe('A new deck', function() {
+  var callbacks = {};
+  var parameters = {};
+  var resetParameters = function() {
+    callbacks = {};
+    parameters = {};
+  };
+
+  it('can be called with no parameters', function(done) {
+    callbacks.success = function(deck) {
+      expect(deck.deck_id).toEqual(jasmine.anything());
+      done();
+    };
+
+    DoC.createDeck(callbacks);
+  });
+
+  it('can be shuffled on initialization', function(done) {
+    callbacks.success = function(deck) {
+      expect(deck.shuffled).toBe(true);
+      done();
+    };
+
+    parameters.shuffle = true;
+    DoC.createDeck(callbacks, parameters);
+  });
+
+  it('can contain multiple decks', function(done) {
+    resetParameters();
+    callbacks.success = function(deck) {
+      expect(deck.remaining).toBe(156);
+      done();
+    };
+
+    parameters.numDecks = 3;
+    DoC.createDeck(callbacks, parameters);
+  });
+
+  it('can contain a partial amount of cards', function(done) {
+    resetParameters();
+    callbacks.success = function(deck) {
+      expect(deck.remaining).toBe(4);
+      done();
+    };
+
+    parameters.partialDeck = [ 'AS', 'AC', 'AD', 'AH' ];
+    DoC.createDeck(callbacks, parameters);
+  });
+
+  it('initialized as a partial deck and multiple decks only includes partial cards', function(done) {
+    resetParameters();
+    callbacks.success = function(deck) {
+      expect(deck.remaining).toBe(3);
+      done();
+    };
+
+    parameters.partialDeck = ['AS', 'AD', 'AH'];
+    parameters.numDecks = 3;
+    DoC.createDeck(callbacks, parameters);
+  });
+});
+
+describe('Drawing from a deck created with no parameters', function() {
+  // deck created with no parameters
+  var deckID;
+  var callbacks = {};
+
+  beforeAll(function(done) {
+    // create the default deck
+    DoC.createDeck({
+      success: function(deck) {
+        deckID = deck.deck_id;
+        done();
+      }
+    });
+  });
+
+  it('only draws one card if no numCards parameter specified', function(done) {
+    var success = function(response) {
+      expect(response.cards.length).toBe(1);
+      done();
+    };
+
+    callbacks.success = success;
+    DoC.drawFromDeck(callbacks, { deckID: deckID });
+  });
+});
